91aaf613c17f3e7c5626d96854c77c60c9b37fde
Simeon <Simeon@Simeon-PC>
2016-03-05 01:20:21 -0500

    Inicializacion con la vista del usuario en el dominio

---
+++ domino/src/main/java/com/valtaks/sistema/domino/ViewUsuario.java
@@ -0,0 +1,91 @@
+
+package com.valtaks.sistema.domino;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Simeon
+ */
+
+public class ViewUsuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private int id;
+    private String nombre;
+    private String usuario;
+    private String password;
+    private int idModulo;
+    private String modulo;
+    private int idEstado;
+    private String estado;
+
+    public ViewUsuario() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getIdModulo() {
+        return idModulo;
+    }
+
+    public void setIdModulo(int idModulo) {
+        this.idModulo = idModulo;
+    }
+
+    public String getModulo() {
+        return modulo;
+    }
+
+    public void setModulo(String modulo) {
+        this.modulo = modulo;
+    }
+
+    public int getIdEstado() {
+        return idEstado;
+    }
+
+    public void setIdEstado(int idEstado) {
+        this.idEstado = idEstado;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+}
